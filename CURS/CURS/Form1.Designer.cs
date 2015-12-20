fff06364734fd141fdf2c6c4b8b0816f297b1726
RoadToExclusivity <dimaoq@gmail.com>
2015-12-20 12:32:15 +0300

    :init_cursach:

---
+++ CURS/CURS/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CURS
+{
+    partial class frmEnrolees
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnu = new System.Windows.Forms.MenuStrip();
+            this.абитуриентыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.изменитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnu
+            // 
+            this.mnu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.абитуриентыToolStripMenuItem});
+            this.mnu.Location = new System.Drawing.Point(0, 0);
+            this.mnu.Name = "mnu";
+            this.mnu.Size = new System.Drawing.Size(264, 24);
+            this.mnu.TabIndex = 0;
+            this.mnu.Text = "menuStrip1";
+            // 
+            // абитуриентыToolStripMenuItem
+            // 
+            this.абитуриентыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьToolStripMenuItem,
+            this.изменитьToolStripMenuItem});
+            this.абитуриентыToolStripMenuItem.Name = "абитуриентыToolStripMenuItem";
+            this.абитуриентыToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.абитуриентыToolStripMenuItem.Text = "Абитуриенты";
+            // 
+            // добавитьToolStripMenuItem
+            // 
+            this.добавитьToolStripMenuItem.Name = "добавитьToolStripMenuItem";
+            this.добавитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.добавитьToolStripMenuItem.Text = "Добавить";
+            // 
+            // изменитьToolStripMenuItem
+            // 
+            this.изменитьToolStripMenuItem.Name = "изменитьToolStripMenuItem";
+            this.изменитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.изменитьToolStripMenuItem.Text = "Изменить";
+            // 
+            // frmEnrolees
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 25);
+            this.Controls.Add(this.mnu);
+            this.MainMenuStrip = this.mnu;
+            this.MaximizeBox = false;
+            this.Name = "frmEnrolees";
+            this.Text = "Абитуриенты";
+            this.mnu.ResumeLayout(false);
+            this.mnu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnu;
+        private System.Windows.Forms.ToolStripMenuItem абитуриентыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem изменитьToolStripMenuItem;
+
+    }
+}
+
