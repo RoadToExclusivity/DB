f1ff848e7060af388b06fc688ee59bb4c9b43479
RoadToExclusivity <dimaoq@gmail.com>
2015-12-29 22:09:15 +0300

    Last commit

--- CURS/CURS/EditEnrollee.cs
+++ CURS/CURS/EditEnrollee.cs
@@ -66,6 +66,7 @@ namespace CURS
             newDataSpecTable.Columns.Add();
             foreach (var specRow in specRows)
             {
+                int ID = Convert.ToInt32(specRow[0].ToString());
                 int specID = Convert.ToInt32(specRow[1].ToString());
                 string name = enroleeDataSet.Specialities.FindByID_специальности(specID)[1].ToString();
                 string val = specRow[2].ToString();
@@ -79,14 +80,19 @@ namespace CURS
                 newRow[0] = name;
                 newRow[1] = val;
                 newDataSpecTable.Rows.Add(newRow);
+
+                var actualSpecRow = enroleeDataSet.EnroleeSpeciality.FindByID_абитуриентаID_специальности(ID, specID);
+                actualSpecRow.Delete();
             }
             Helper.tempAddSpec = newDataSpecTable;
+            enroleeSpecAdapter.Update(enroleeDataSet.EnroleeSpeciality);
 
             DataTable newDataExamTable = new DataTable();
             newDataExamTable.Columns.Add();
             newDataExamTable.Columns.Add();
             foreach (var examRow in examRows)
             {
+                int ID = Convert.ToInt32(examRow[0].ToString());
                 int examID = Convert.ToInt32(examRow[1].ToString());
                 string name = enroleeDataSet.Exams.FindByID_экзамена(examID)[1].ToString();
                 string val = examRow[2].ToString();
@@ -96,8 +102,12 @@ namespace CURS
                 newRow[0] = name;
                 newRow[1] = val;
                 newDataExamTable.Rows.Add(newRow);
+
+                var actualSpecRow = enroleeDataSet.EnrolleeExams.FindByID_абитуриентаID_экзамена(ID, examID);
+                actualSpecRow.Delete();
             }
             Helper.tempAddExam = newDataExamTable;
+            enrExamAdapter.Update(enroleeDataSet.EnrolleeExams);
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
@@ -107,6 +117,66 @@ namespace CURS
             {
                 return;
             }
+
+            var row = enroleeDataSet.Enrolee.FindByID_абитуриента(Helper.enrID);
+            row[1] = txtF.Text;
+            row[2] = txtI.Text;
+            row[3] = txtO.Text;
+
+            enroleeAdapter.Update(enroleeDataSet.Enrolee);
+
+            foreach (var item in lstSpec.Items)
+            {
+                var newSpec = enroleeDataSet.EnroleeSpeciality.NewEnroleeSpecialityRow();
+                newSpec.ID_абитуриента = Convert.ToInt32(enroleeDataSet.Enrolee.Rows[enroleeDataSet.Enrolee.Rows.Count - 1][0].ToString());
+
+                string[] spl = item.ToString().Split('-');
+                string spec = spl[0].Substring(0, spl[0].Length - 1);
+                string priorStr = spl[1].Substring(1, spl[1].Length - 1);
+                int prior = 0;
+                if (priorStr != "MAX")
+                {
+                    prior = Convert.ToInt32(priorStr);
+                }
+
+                foreach (var rowSpec in enroleeDataSet.Specialities)
+                {
+                    if (rowSpec.Название_специальности == spec)
+                    {
+                        newSpec.ID_специальности = rowSpec.ID_специальности;
+                        break;
+                    }
+                }
+                newSpec.Приоритет = prior;
+
+                enroleeDataSet.EnroleeSpeciality.Rows.Add(newSpec);
+            }
+            enroleeSpecAdapter.Update(enroleeDataSet.EnroleeSpeciality);
+
+            foreach (var item in lstExam.Items)
+            {
+                var newExam = enroleeDataSet.EnrolleeExams.NewEnrolleeExamsRow();
+                newExam.ID_абитуриента = Convert.ToInt32(enroleeDataSet.Enrolee.Rows[enroleeDataSet.Enrolee.Rows.Count - 1][0].ToString());
+
+                string[] spl = item.ToString().Split('-');
+                string exam = spl[0].Substring(0, spl[0].Length - 1);
+                int ball = Convert.ToInt32(spl[1].Substring(1, spl[1].Length - 1));
+
+                foreach (var rowExam in enroleeDataSet.Exams)
+                {
+                    if (rowExam.Название_экзамена == exam)
+                    {
+                        newExam.ID_экзамена = rowExam.ID_экзамена;
+                        break;
+                    }
+                }
+                newExam.Количество_баллов_за_экзамен = ball;
+
+                enroleeDataSet.EnrolleeExams.Rows.Add(newExam);
+            }
+            enrExamAdapter.Update(enroleeDataSet.EnrolleeExams);
+
+            this.Close();
         }
 
         private void btnSpec_Click(object sender, EventArgs e)
